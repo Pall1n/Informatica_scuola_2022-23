7e3bd9282ae9dc4e2e1ced0cb8b164c50e9a5248
Pall1n <75300377+Pall1n@users.noreply.github.com>
2022-10-08 19:15:30 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+/*
+    Implementare la classe conto corrente per la gestione 
+    dei depositi eprelievi in un c.c. bancario. La classe 
+    dovrà avere i metodi:
+    1) Deposita
+    2) Prelieva
+    3) Cambia intestatario conto
+    4) Costruttore
+*/
+
+public class Main {
+	public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        Login login = new Login();
+        login.setVisible(true);
+        login.setResizable(false);
+	}
+}
