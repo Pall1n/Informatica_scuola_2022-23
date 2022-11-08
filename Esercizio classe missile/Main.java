9ebb302e1bed76274f135b930701e84cf2884b7e
Pall1n <75300377+Pall1n@users.noreply.github.com>
2022-11-08 12:20:27 +0200

    Update Main.java

--- Esercizio classe missile/Main.java
+++ Esercizio classe missile/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Missile missile = new Missile(new int[] {20,20}, new int[] {3,5}, 2);
+        Missile missile = new Missile(new int[] {10,10}, new int[] {3,5}, 2, "terra aria");
         missile.launch();
     }
 }
