59e265e8c4a5f59c15b12e44282cf24496feda5d
Pall1n <paolomarialarosa@gmail.com>
2023-02-02 21:05:14 +0100

    Updated output

--- Esercizi GUI input-output/Output.java
+++ Esercizi GUI input-output/Output.java
@@ -1,20 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Scanner;
 
 public class Output extends JFrame {
-    public Output() {
+    public Output(String nome) {
         setTitle("Output semplice");
         setSize(300, 200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         panel.setBackground(Color.LIGHT_GRAY);
         add(panel);
-        JLabel label = new JLabel("Ciao mondo!");
+        JLabel label = new JLabel("Ciao " + nome + "!");
         panel.add(label);
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        new Output();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inserisci il tuo nome: ");
+        String nome = input.nextLine();
+        new Output(nome);
+        input.close();
     }
 }
