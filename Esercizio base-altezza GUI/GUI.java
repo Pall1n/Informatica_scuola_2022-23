c8283a738a54ee3166cb6f9d736c9e9bb88685c6
Paolo Maria Larosa <75300377+Pall1n@users.noreply.github.com>
2023-03-31 09:30:17 +0200

    Add files via upload

---
+++ Esercizio base-altezza GUI/GUI.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GUI extends JFrame{
+    private JPanel panel;
+    private double baseValue, altezzaValue, areaTriangolo, perimetroRett, areaRett;
+
+    public GUI() {
+        super("Base-Altezza");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(200, 140);
+        setLayout(new FlowLayout());
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 2));
+        JLabel baseText = new JLabel("Base: ");
+        JTextField baseInput = new JTextField();
+        JLabel altezzaText = new JLabel("Altezza: ");
+        JTextField altezzaInput = new JTextField();
+        JButton btn1 = new JButton("CALCOLA");
+        panel.add(baseText);
+        panel.add(baseInput);
+        panel.add(altezzaText);
+        panel.add(altezzaInput);
+        panel.add(btn1);
+        add(panel);
+        setVisible(true);
+        setResizable(false);
+
+        btn1.addActionListener(e -> {
+            try {
+                baseValue = Integer.parseInt(baseInput.getText());
+                altezzaValue = Integer.parseInt(altezzaInput.getText());
+                areaTriangolo = (baseValue * altezzaValue) / 2;
+                perimetroRett = (baseValue + altezzaValue) * 2;
+                areaRett = baseValue * altezzaValue;
+                JOptionPane.showMessageDialog(null, "Area triangolo: " + areaTriangolo + "; perimetro rettangolo: " + perimetroRett + "; area rettangolo: " + areaRett + ".", "Risultati", JOptionPane.INFORMATION_MESSAGE);
+            } catch(NumberFormatException err) {
+                JOptionPane.showMessageDialog(null, "Inserisci numeri!", "Errore", JOptionPane.WARNING_MESSAGE);
+            }
+        });
+    }
+
+}
---
+++ Esercizio base-altezza GUI/Main.java
@@ -0,0 +1,10 @@
+/*
+Implementare una GUI in Java che dati due valori in ingresso, base e altezza, stampi 
+l'area del triangolo,  e il perimetro e l'area del rettangolo.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
